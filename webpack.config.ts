2a8c3a4e7c8b9d0adaa24a983de89030fed2fdaa
Halvard Mørstad <halvard@simplyundoable.com>
2020-12-21 18:59:43 +0100

    Added core

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,168 +1,194 @@
 import path from "path";
 import webpack from "webpack";
-import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import Dotenv from 'dotenv-webpack'
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import Dotenv from "dotenv-webpack";
+import env from "dotenv";
+import GoogleFontsPlugin from "google-fonts-plugin";
 
 //PostCss plugins
-import purgecss from '@fullhuman/postcss-purgecss'
-import postcssPresetEnv from 'postcss-preset-env'
-import cssNano from 'cssnano'
-import autoprefixer from 'autoprefixer'
-import atImport from 'postcss-import'
+import purgecss from "@fullhuman/postcss-purgecss";
+import postcssPresetEnv from "postcss-preset-env";
+import cssNano from "cssnano";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
+import atImport from "postcss-import";
 // @ts-ignore
-import purgefonts from 'postcss-purgefonts'
+import purgefonts from "postcss-purgefonts";
 
-const pathDist = path.resolve(__dirname, "dist")
-const pathPublic = path.resolve(__dirname, "public")
-const pathSource = path.resolve(__dirname, "src")
-const pathModules = path.resolve(__dirname, "node_modules")
-const pathTsConfig = path.resolve(__dirname, "tsconfig.json")
+env.config();
+
+const pathDist = path.resolve(__dirname, "dist");
+const pathPublic = path.resolve(__dirname, "public");
+const pathSource = path.resolve(__dirname, "src");
+const pathModules = path.resolve(__dirname, "node_modules");
+const pathTsConfig = path.resolve(__dirname, "tsconfig.json");
+
+const isProd = process.env.NODE_ENV === "production";
 
 const postcssConfig = (isProd: boolean) => ({
-    ident: 'postcss',
-    plugins: [
-        atImport(),
-        postcssPresetEnv(),
-        autoprefixer(),
-        isProd && cssNano({
-            preset: 'default',
-        }),
-        isProd && purgecss({
-            content: [
-                './src/**/*.html',
-                './src/**/*.ts',
-                './src/**/*.tsx',
-                './public/index.html',
+  ident: "postcss",
+  plugins: [
+    atImport(),
+    postcssPresetEnv(),
+    tailwind(),
+    autoprefixer(),
+    isProd && cssNano({
+      preset: "default",
+    }),
+    isProd && purgecss({
+      content: [
+        "./src/**/*.html",
+        "./src/**/*.ts",
+        "./src/**/*.tsx",
+        "./public/index.html",
+      ],
+      defaultExtractor: (content) => {
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) ||
+          [];
+        return broadMatches.concat(innerMatches);
+      },
+      fontFace: true,
+      safelist: ["html", "body"],
+    }),
+    isProd && purgefonts({
+      to: path.join(pathDist, "fonts"),
+    }),
+  ].filter(Boolean),
+});
+
+const result: webpack.Configuration = {
+  context: pathSource,
+  entry: "./index.tsx",
+  devtool: isProd ? "source-map" : "eval",
+  resolve: {
+    extensions: [".tsx", ".jsx", ".ts", ".js", ".pcss", ".scss", ".css"],
+    modules: [pathModules, pathSource],
+  },
+  output: {
+    path: pathDist,
+    filename: "bundle.[hash].js",
+  },
+  devServer: {
+    contentBase: pathPublic,
+    compress: true,
+    historyApiFallback: true,
+    // @ts-ignore
+    port: process.env.DEV_SERVER_PORT,
+    stats: {
+      children: false,
+      modules: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
             ],
-            defaultExtractor: (content) => {
-                const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-                const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-                return broadMatches.concat(innerMatches)
+            plugins: [
+              [
+                "@babel/plugin-transform-runtime",
+                {
+                  "regenerator": true,
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.pcss$/,
+        exclude: /node_modules/,
+        // @ts-ignore
+        use: [
+          isProd ? MiniCssExtractPlugin.loader : "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
             },
-            fontFace: true,
-            safelist: ['html', 'body'],
-        }),
-        isProd && purgefonts({
-            to: path.join(pathDist, 'fonts')
-        }),
-    ].filter(Boolean),
-})
-
-const config: webpack.ConfigurationFactory = (env, argv) => {
-    const nodeEnv = argv.mode === 'production' ? 'production' : 'development';
-    const isProd = nodeEnv === 'production';
-
-    const result: webpack.Configuration = {
-        context: pathSource,
-        entry: "./index.tsx",
-        devtool: isProd ? 'source-map' : 'eval',
-        resolve: {
-            extensions: [".tsx", ".jsx", ".ts", ".js", ".pcss", ".scss", ".css"],
-            modules: [pathModules, pathSource]
+          },
+          !isProd && "resolve-url-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              postcssOptions: postcssConfig(isProd),
+            },
+          },
+        ].filter(Boolean),
+      },
+      {
+        test: /\.(ttf|eot|svg|gif|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // @ts-ignore
+    new Dotenv({ systemvars: true, silent: true }),
+    // @ts-ignore
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: "./**/*.{ts,tsx,js,jsx}",
+      },
+      typescript: {
+        configFile: pathTsConfig,
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(pathPublic, "index.html"),
+    }),
+    new GoogleFontsPlugin({
+      fonts: [
+        {
+          family: "Roboto",
+          variants: ["400"],
         },
-        output: {
-            path: pathDist,
-            filename: "bundle.[hash].js",
+        {
+          family: "Noto Sans",
+          variants: ["400"],
         },
-        devServer: {
-            contentBase: pathPublic,
-            compress: true,
-            port: 4100,
-            stats: {
-                children: false,
-                modules: false,
-            }
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.[tj]sx?$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            presets: [
-                                "@babel/preset-env",
-                                "@babel/preset-react",
-                                "@babel/preset-typescript",
-                            ],
-                        },
-                    },
-                },
-                {
-                    test: /\.pcss$/,
-                    exclude: /node_modules/,
-                    // @ts-ignore
-                    use: [
-                        isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                importLoaders: 1,
-                            },
-                        },
-                        !isProd && 'resolve-url-loader',
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: true,
-                                postcssOptions: postcssConfig(isProd)
-                            }
-                        }
-                    ].filter(Boolean),
-                },
-                {
-                    test: /\.(ttf|eot|svg|gif|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: '[name].[ext]',
-                                outputPath: 'fonts/'
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.(png|jpg)$/,
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: '[name].[ext]',
-                                outputPath: 'images/'
-                            }
-                        }
-                    ]
-                },
-            ],
-        },
-        plugins: [
-            new Dotenv({systemvars: true}),
-            new webpack.EnvironmentPlugin({
-                NODE_ENV: nodeEnv,
-            }),
-            new MiniCssExtractPlugin({
-                filename: "[name].css",
-            }),
-            new ForkTsCheckerWebpackPlugin({
-                async: false,
-                eslint: {
-                    files: "./**/*.{ts,tsx,js,jsx}"
-                },
-                typescript: {
-                    configFile: pathTsConfig,
-                },
-            }),
-            new HtmlWebpackPlugin({
-                template: path.join(pathPublic, 'index.html'),
-            }),
-        ]
-    }
+      ],
+      formats: ["woff"],
+      encode: true,
+      cache: true,
+    }),
+  ],
+};
 
-    return result
-}
-
-export default config
+export default result;
