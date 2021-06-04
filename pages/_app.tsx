b6879cd7a25efa6eb981090824fc16c6e84b7cbb
Halvard Mørstad <halvard@simplyundoable.com>
2021-06-04 11:00:11 +0200

    Added emotion and twin.macro

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Fragment } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+function App({ Component, pageProps }: AppProps) {
+  return <Fragment>
+    <Component {...pageProps} />
+  </Fragment>
 }
-export default MyApp
+export default App
