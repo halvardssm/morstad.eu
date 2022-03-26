a60ef7b002081b6b56369fb4b663d74949c68c44
Halvard Mørstad <halvard@simplyundoable.com>
2022-03-26 09:16:57 +0000

    Bumped versions and introduced pnp

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,13 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="@emotion/react/types/css-prop" />
+/// <reference types="next/image-types/global" />
 
-import "twin.macro";
-import styledImport from "@emotion/styled";
-import { css as cssImport } from "@emotion/react";
-
-declare module "twin.macro" {
-  // The styled and css imports
-  const styled: typeof styledImport;
-  const css: typeof cssImport;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  purge: {
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    options: {
-      safelist: [/(bg|text|border)-(.*)-(\d{1}0{1,2})/],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  safelist: [
+    {
+      pattern:/(bg|text|border)-(.*)-(\d{1}0{1,2})/,
     },
-  },
+  ],
   darkMode: "class",
   theme: {
     extend: {},
