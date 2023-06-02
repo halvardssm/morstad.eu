cd1805d36cf7120fd2f86ad3795c59bae5c5759f
Halvard Mørstad <halvard@simplyundoable.com>
2023-06-02 10:16:12 +0200

    Added i18n config

---
+++ src/i18n/config.ts
@@ -0,0 +1,26 @@
+import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import HttpApi from "i18next-http-backend";
+import { IS_DEV } from "../variables";
+
+export const supportedLanguages = ["en","no","de"];
+
+i18n
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    returnNull: false,
+    backend: {
+      loadPath: "/locales/{{lng}}.json",
+    },
+    lng: supportedLanguages[0],
+    fallbackLng: supportedLanguages,
+    debug: IS_DEV,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
