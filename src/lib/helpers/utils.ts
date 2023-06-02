03eccedb260810a4d1613998e19fc38499723de9
Halvard Mørstad <halvard@simplyundoable.com>
2023-06-02 10:17:26 +0200

    added helpers

---
+++ src/lib/helpers/utils.ts
@@ -0,0 +1,27 @@
+import { ContentFolder, PostT, ProjectT } from "../../types";
+
+export type ContentData = {
+  posts: PostT[];
+  projects: ProjectT[];
+};
+
+export const fetcher = (input: RequestInfo | URL, init?: RequestInit) =>
+  fetch(input, init).then((res) => res.json());
+
+export const parseContentData = () => {
+  const contentData = __CONTENT_DATA__; //JSON.parse(__CONTENT_DATA__);
+  return contentData as unknown as ContentData;
+};
+
+export function arrayLoopAround<T>(array: T[], index: number): T {
+  const arrayLength = array.length;
+
+  if (index < arrayLength && index >= 0) {
+    return array[index];
+  }
+
+  const adjustedArrayIndex =
+    index - Math.floor(index / arrayLength) * arrayLength;
+
+  return array[adjustedArrayIndex];
+}
